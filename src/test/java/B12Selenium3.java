91f02561bc17274eb0d99aae10f912bf81905b3b
Slava <viacheslav.verba@gmail.com>
2023-10-30 01:58:16 -0700

    Fourth Commit

--- src/test/java/B12Selenium3.java
+++ src/test/java/B12Selenium3.java
@@ -1,7 +1,6 @@
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import java.time.Duration;
