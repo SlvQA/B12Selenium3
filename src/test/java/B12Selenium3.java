9e0126937a4c925d724a464faa5b2a6ec0999166
Slava <viacheslav.verba@gmail.com>
2023-10-29 19:55:17 -0700

    Third Commit

--- src/test/java/B12Selenium3.java
+++ src/test/java/B12Selenium3.java
@@ -12,6 +12,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.openqa.selenium.support.ui.ExpectedConditions.elementToBeClickable;
+import static org.openqa.selenium.support.ui.ExpectedConditions.textToBePresentInElementValue;
+
 public class B12Selenium3 {
     @Test
     public void navigateToWebsite() {
@@ -22,7 +25,7 @@ public class B12Selenium3 {
             //navigate to the webpage
             driver.manage().window().maximize();
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-            //            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
             //            wait.until(ExpectedConditions.elementToBeClickable(yearMin));
             driver.get("https://www.edmunds.com/");
 
@@ -37,25 +40,25 @@ public class B12Selenium3 {
             //erasing and changing ZIP
             WebElement zipField = driver.findElement(By.xpath("//input[@name='zip']"));
             zipField.click();
-            zipField.sendKeys(Keys.chord(Keys.CONTROL,"a"), Keys.DELETE);
+            zipField.sendKeys(Keys.chord(Keys.CONTROL, "a"), Keys.DELETE);
             zipField.sendKeys("22031", Keys.ENTER);
 
             //clicking the checkBox for Local Listings only
             WebElement checkBoxLocalListings = driver.findElement(By.xpath("(//input[@type='checkbox'])[2]"));
-            if (checkBoxLocalListings.isDisplayed() && !checkBoxLocalListings.isSelected()){
+            if (checkBoxLocalListings.isDisplayed() && !checkBoxLocalListings.isSelected()) {
                 checkBoxLocalListings.click();
             }
 
-        //Assert.assertTrue(checkBoxLocalListings.isSelected()); //- DOESN'T WORK
+            //Assert.assertTrue(checkBoxLocalListings.isSelected()); //- DOESN'T WORK
 
             //choosing Make of the car
             WebElement makeDropDown = driver.findElement(By.xpath("//select[@id='usurp-make-select']"));
-            Select makeSelect = new Select (makeDropDown);
+            Select makeSelect = new Select(makeDropDown);
             makeSelect.selectByVisibleText("Tesla");
 
             //verifying that Model is not selected
             WebElement modelDropDown = driver.findElement(By.xpath("//select[@id='usurp-model-select']"));
-            Select modelSelect = new Select (modelDropDown);
+            Select modelSelect = new Select(modelDropDown);
             String actualModel = modelSelect.getFirstSelectedOption().getText();
             String expectedModel = "Add Model";
             Assert.assertEquals(actualModel, expectedModel);
@@ -77,9 +80,9 @@ public class B12Selenium3 {
             ArrayList<String> actualModelsList = new ArrayList<>();
             List<String> expectedModelList = Arrays.asList("Add Model", "Model 3", "Model S", "Model X", "Model Y", "Cybertruck", "Roadster");
             List<WebElement> modelOptions = modelSelect.getOptions();
-            for(WebElement option : modelOptions) {
+            for (WebElement option : modelOptions) {
                 actualModelsList.add(option.getText());
-                }
+            }
 //            System.out.println(actualModelsList);
 //            System.out.println(expectedModelList);
             Assert.assertEquals(actualModelsList, expectedModelList);
@@ -99,25 +102,63 @@ public class B12Selenium3 {
             driver.navigate().refresh();
 
             //verify that there are 21 search results, excluding the sponsored result(s).
-            List <WebElement> listOfResults = driver.findElements(By.xpath("//li[@class='d-flex mb-0_75 mb-md-1_5 col-12 col-md-6']"));
-                int actualListSize = listOfResults.size();
-                int expectedSize = 21;
-                Assert.assertEquals(actualListSize, expectedSize);
-                System.out.println(actualListSize);
+            List<WebElement> listOfResults = driver.findElements(By.xpath("//li[@class='d-flex mb-0_75 mb-md-1_5 col-12 col-md-6']"));
+            int actualListSize = listOfResults.size();
+            int expectedSize = 21;
+            Assert.assertEquals(actualListSize, expectedSize);
+            //System.out.println(actualListSize);
 
             //verify that each search result title contains ‘Tesla Model 3’ & verify that each year is within the selected range (2020-2023)
-            List <WebElement> titles = driver.findElements(By.xpath("//li[@class='d-flex mb-0_75 mb-md-1_5 col-12 col-md-6']//img"));
+            List<WebElement> titles = driver.findElements(By.xpath("//li[@class='d-flex mb-0_75 mb-md-1_5 col-12 col-md-6']//img"));
             String searchTermTesla = "Tesla Model 3";
-            for (WebElement title : titles){
+            for (WebElement title : titles) {
                 //System.out.println(title.getAttribute("alt"));
-                int year = Integer.parseInt(title.getAttribute("alt").substring(0,4));
-                System.out.println(year);
+                int year = Integer.parseInt(title.getAttribute("alt").substring(0, 4));
+                //System.out.println(year);
                 Assert.assertTrue(title.getAttribute("alt").toLowerCase().contains(searchTermTesla.toLowerCase()));
-                assert(2020 <= year && year <= 2023);
+                assert (2020 <= year && year <= 2023);
             }
 
+            //From the dropdown on choose “Price: Low to High"
+            WebElement sortDropDown = driver.findElement(By.xpath("//select[@id='sort-by']"));
+            Select select = new Select(sortDropDown);
+            select.selectByVisibleText("Price: Low to High");
+            driver.navigate().refresh();
 
 
+            //verify that the results are sorted from lowest to highest price.
+            List<WebElement> findPrices = driver.findElements(By.xpath("//div[@class='pricing-details d-flex flex-column']//span[@class='heading-3']"));
+            List<Integer> pricesToVerify = new ArrayList<>();
+            for (WebElement p : findPrices) {
+                String price = p.getText().split(" ")[0];
+                price = price.replace("$", "").replace(",", "");
+                pricesToVerify.add(Integer.parseInt(price));
+                //System.out.println(pricesToVerify);
+            }
+            int int1;
+            for (int1 = 1; int1 <= pricesToVerify.size()-1; int1++) {
+                Assert.assertTrue(pricesToVerify.get(int1-1) <= pricesToVerify.get(int1), "Ascending order is incorrect.");
+            }
+
+            //sort by "Price High to Low"
+            WebElement sortDropDown1 = driver.findElement(By.xpath("//select[@id='sort-by']"));
+            Select select2 = new Select(sortDropDown1);
+            select2.selectByVisibleText("Price: High to Low");
+            driver.navigate().refresh();
+
+            //verify that the results are sorted from highest to lowest price.
+            List<WebElement> findPrices1 = driver.findElements(By.xpath("//div[@class='pricing-details d-flex flex-column']//span[@class='heading-3']"));
+            List<Integer> pricesToVerify1 = new ArrayList<>();
+            for (WebElement p1 : findPrices1) {
+                String price1 = p1.getText().split(" ")[0];
+                price1 = price1.replace("$", "").replace(",", "");
+                pricesToVerify1.add(Integer.parseInt(price1));
+                //System.out.println(pricesToVerify1);
+            }
+            int int2;
+            for (int2 = 1; int2 <= pricesToVerify.size()-1; int2++) {
+                Assert.assertTrue(pricesToVerify1.get(int2-1) >= pricesToVerify1.get(int2), "Descending order is incorrect.");
+            }
 
 
 
